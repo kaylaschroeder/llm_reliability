9f2e0ba1e5f6f78b6af5b41bea5e7185a05a2c45
Kayla Schroeder <kaylaaschroeder@gmail.com>
2024-10-21 13:12:21 -0500

    Restructuring

--- judges.py
+++ judges.py
@@ -7,19 +7,13 @@ import torch
 from utils import generate_judge
 
 
-device = "cuda:0"
+device = "cuda:1"
 
 # Hugging face access token
 access_token = ''
 
 # Define LLM judge types
-model_types = ['meta-llama/Meta-Llama-3-8B',
-#               'mistralai/Mistral-7B-v0.1',
-               'lmsys/vicuna-7b-v1.5',
-               'google/gemma-7b',
-               'bigscience/bloom-7b1',
-               'microsoft/phi-2',
-               'tiiuae/falcon-7b']
+model_types = ['Nexusflow/Starling-LM-7B-beta']
                
 
 # Load data for judgement
@@ -30,10 +24,10 @@ with open('prompt_responses.json', 'r') as resp:
 random.seed(2)
 judge_seeds = random.sample(range(1,10000),100)
 
-judge_prompt = """You are a fair and objective judge tasked with selecting the strongest of the following responses to the provided question. Base your judgement upon the accuracy, utility, and relevance of each. Do not consider length of response, positioning of response or title of response in your judgement. Output exclusively the letter of the best response and nothing else.
+judge_prompt = """You are a fair and objective judge tasked with selecting the strongest of the following responses to the provided question. Base your judgement upon the accuracy, utility, and relevance of each. Do not consider length of response, positioning of response or title of response in your judgement. Output the letter of the best response followed by an explanation, and strictly follow the following format: "[[letter]]", for example: "Best Response: [[C]]".
 """
 
-judge_prompt_multi = """You are a fair and objective judge tasked with selecting the strongest of the following responses to the second provided question ("Question 2"). Question 1 is only provided for context. Base your judgement upon the accuracy, utility, and relevance of each. Do not consider length of response, positioning of response or title of response in your judgement. Output exclusively the letter of the best response and nothing else.
+judge_prompt_multi = """You are a fair and objective judge tasked with selecting the strongest of the following responses to the second provided question ("Question 2"). Question 1 is only provided for context. Base your judgement upon the accuracy, utility, and relevance of each. Do not consider length of response, positioning of response or title of response in your judgement. Output the letter of the best response followed by an explanation, and strictly follow the following format: "[[letter]]", for example: "Best Response: [[C]]".
 """
 
 prompt_resp_formatted = {}
@@ -54,33 +48,39 @@ def model_judge(model_name, prompts_judge, access_token):
   tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token)
   model = AutoModelForCausalLM.from_pretrained(model_name, token=access_token)
 
-  model_judge_list = []
-  for prompt_dict in prompts_judge:
-    judge_bbh = [[generate_judge(model, tokenizer, judge_prompt+"\nQuestion: "+\
-      q_dict['prompt']+"Responses: \n[A]: "+\
-      q_dict['model_responses'][0]+\
-      "\n[B]: "+ q_dict['model_responses'][1]+ \
-      "\n[C]: "+ q_dict['model_responses'][2]+\
-      "\n[D]: "+ q_dict['model_responses'][3]+\
-      "\n[E]: "+ q_dict['model_responses'][4],
-      indiv_seed, device) for indiv_seed in judge_seeds]\
-      for q, q_dict in prompts_judge.items() if q_dict['type'] == 'single']
-    mod_print_name = model_name.replace('/', '_')
-    with open(f'judge_bbh_{mod_print_name}.json','w') as f:
-          json.dump(judge_bbh, f)
-    judge_mt = [[generate_judge(judge_prompt_multi+"\nQuestion 1: "+q_dict['prompt'][0]+\
-      "\nQuestion 2: "+q_dict['prompt'][1]+\
-      "Responses: \n[A]: "+ q_dict['model_responses'][0]+\
-      "\n[B]: "+q_dict['model_responses'][1]+ \
-      "\n[C]: "+q_dict['model_responses'][2]+\
-      "\n[D]: "+q_dict['model_responses'][3]+\
-      "\n[E]: "+q_dict['model_responses'][4],
-      indiv_seed, device) for indiv_seed in judge_seeds]\
-    for q, q_dict in prompts_judge.items() if q_dict['type']== 'multi']
-    # Save outputs to json
-    with open(f'judge_mt_{mod_print_name}.json', 'w') as f:
-        json.dump(judge_mt, f)
-  return [judge_bbh,judge_mt]
+  model_judge_dict = {}
+  bbh_model_judge_dict = {}
+  mt_model_judge_dict = {}
+  for q, q_dict in prompts_judge.items():
+    if q_dict['type'] == 'single':
+        mod_judge = [generate_judge(model, tokenizer, judge_prompt+"\nQuestion: "+ \
+        q_dict['prompt']+"Responses: \n[A]: "+ q_dict['model_responses'][0]+\
+        "\n[C]: "+ q_dict['model_responses'][2]+ \
+        "\n[D]: "+ q_dict['model_responses'][3]+ \
+        "\n[E]: "+ q_dict['model_responses'][4],
+        indiv_seed, device) for indiv_seed in judge_seeds]
+        bbh_model_judge_dict[q] = mod_judge
+        model_judge_dict[q] = mod_judge
+    else:
+        mod_judge = [generate_judge(model, tokenizer, judge_prompt_multi+"\nQuestion 1: "+\
+        q_dict['prompt'][0]+"\nQuestion 2: "+q_dict['prompt'][1]+\
+        "Responses: \n[A]: "+ q_dict['model_responses'][0]+\
+        "\n[B]: "+q_dict['model_responses'][1]+ \
+        "\n[C]: "+q_dict['model_responses'][2]+\
+        "\n[D]: "+q_dict['model_responses'][3]+\
+        "\n[E]: "+q_dict['model_responses'][4],
+        indiv_seed, device) for indiv_seed in judge_seeds]
+        mt_model_judge_dict[q] = mod_judge
+        model_judge_dict[q] = mod_judge
+  mod_print_name = model_name.replace('/', '_')
+  # Save outputs to json
+  with open(f'judge_bbh_{mod_print_name}.json','w') as f:
+    json.dump(bbh_model_judge_dict, f)
+  with open(f'judge_mt_{mod_print_name}.json', 'w') as f:
+    json.dump(mt_model_judge_dict, f)
+  with open(f'judge_full_{mod_print_name}.json', 'w') as f:
+    json.dump(model_judge_dict, f)
+  return model_judge_dict
 
 
 judgements_by_mod = {}

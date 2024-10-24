14e25893e866c5f9d0200a29e0566e75ce3e088b
Kayla Schroeder <kaylaaschroeder@gmail.com>
2024-10-24 14:58:29 -0500

    Adding squad

--- resp_accuracy.py
+++ resp_accuracy.py
@@ -1,14 +1,21 @@
 import json
 import pandas as pd
-import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('punkt')
 
 with open('bbh_sampled_q.json') as s:
   bbh_samp = json.load(s)
+with open('squad_sampled_q.json') as sq:
+  squad_samp = json.load(sq)
 with open('prompt_responses.json') as r:
   responses = json.load(r)
 
+# BBH
 # Match labels in sampled questions to response labels
-resp_target_match = []
+bbh_resp_target_match = []
 for q_lst in bbh_samp:
   curr_target_match = []
   target = q_lst['target']
@@ -34,11 +41,33 @@ for q_lst in bbh_samp:
             curr_target_match.append(0)
       else:
         curr_target_match.append(0)
-    resp_target_match.append(pd.DataFrame({'single:'+q_lst['category']: curr_target_match}))
+    bbh_resp_target_match.append(pd.DataFrame({'single:'+q_lst['category']: curr_target_match}))
     
-result = pd.concat(resp_target_match, axis=1)
-result.loc[3,'single:date_understanding'] = 1
-result.loc['prompt_accuracy'] = result.sum(axis=0)
+bbh_result = pd.concat(resp_target_match, axis=1)
+bbh_result.loc[3,'single:date_understanding'] = 1
+bbh_result.loc['prompt_accuracy'] = bbh_result.sum(axis=0)
 
-result.to_csv('accuracy_resp.csv')
-    
+bbh_result.to_csv('accuracy_resp.csv')
+
+# SQuAD
+squad_resp_target_match = []
+stop_words = set(stopwords.words('english'))
+for label,resp_dict in responses.items():
+  curr_target_match = []
+  if label.startswith('squad'):
+    # Tokenize the answer
+    true_ans = [prompt['answers'][0]['text'] for prompt in squad_samp if label == ('squad:'+prompt['id'])][0]
+    true_ans_tokens = nltk.word_tokenize(true_ans)
+    true_ans_tokens = [[word for word in ans if word not in stop_words] for ans in true_ans]
+    for mod, mod_resp in resp_dict['model_responses'].items():
+      # Tokenize the response
+      mod_resp_tokens = nltk.word_tokenize(mod_resp)
+      mod_resp_tokens = [word for word in mod_resp_tokens if word not in stop_words]
+      if true_ans[0] in mod_resp:
+        curr_target_match.append(1)
+      else:
+        curr_target_match.append(0)
+    squad_resp_target_match.append(pd.DataFrame({label: curr_target_match}))
+squad_result = pd.concat(squad_resp_target_match, axis=1)
+squad_result.loc['prompt_accuracy'] = squad_result.sum(axis=0)
+squad_result.to_csv('accuracy_resp_squad.csv')

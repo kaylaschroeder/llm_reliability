d6237538940652356de5a03038767a55c381595b
Kayla Schroeder <kaylaaschroeder@gmail.com>
2024-11-24 22:04:30 -0600

    Judgments update

--- utils.py
+++ utils.py
@@ -23,9 +23,8 @@ def generate_txt(model, tokenizer, question, seed_val, device):
   generated_text = tokenizer.decode(generated_tensor[0], skip_special_tokens=True)
   answer = generated_text.replace(question, "").strip()
   return answer
-  
-  
-def generate_judge(model, tokenizer, question, seed_val, device):
+
+def generate_judge(model, tokenizer, question, temp, device):
   """
   Generates judgement using the given model, tokenizer, question, and seed value.
   
@@ -33,17 +32,17 @@ def generate_judge(model, tokenizer, question, seed_val, device):
     model (transformers.PreTrainedModel): The pre-trained language model.
     tokenizer (transformers.PreTrainedTokenizer): The tokenizer for the model.
     question (str): The question to generate a response for.
-    seed_val (int): The random seed value.
+    temp (float): The value specified for the temperature parameter.
 
   Returns:
     str: The generated text response.
   """
-  set_seed(seed_val)
   model.to(device)
   inputs = tokenizer(question, return_tensors="pt").to(device)
-  generated_tensor = model.generate(**inputs, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True, temperature = 0.75, top_k=50)
+  generated_tensor = model.generate(**inputs, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id, do_sample=True, temperature = temp, top_k=50)
   generated_tensor = generated_tensor.to(device)
   generated_text = tokenizer.decode(generated_tensor[0], skip_special_tokens=True)
   answer = generated_text.replace(question, "").strip()
   return answer
+
 

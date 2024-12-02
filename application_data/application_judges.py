7045a97606a91590abe35725f6f364bcbaf9da60
Kayla Schroeder <kaylaaschroeder@gmail.com>
2024-12-02 09:48:09 -0600

    Application scripts

---
+++ application_data/application_judges.py
@@ -0,0 +1,96 @@
+import random
+import json
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import set_seed
+import torch
+
+from utils import generate_judge
+
+
+device = "cuda:1"
+
+# Hugging face access token
+access_token = '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'
+
+# Define LLM judge types
+model_types = ['Nexusflow/Starling-LM-7B-beta', 'meta-llama/Meta-Llama-3-8B-Instruct', 'google/gemma-1.1-7b-it']
+               
+
+# Load data for judgement
+with open('application_responses.json', 'r') as resp:
+    prompt_resp = json.load(resp)
+
+random.seed(2)
+
+few_shot_prefix = """You need to check whether the prediction of a question-answering systemto a question is correct. You should make the judgment based on a list of ground truth answers provided to you. Your response should be "correct" if the prediction is correct or "incorrect" if the prediction is wrong.
+Question: Who authored The Taming of the Shrew (published in 2002)?
+Ground truth: ["William Shakespeare", "Roma Gill"]
+Prediction: W Shakespeare
+Correctness: correct
+Question: Who authored The Taming of the Shrew (published in 2002)?
+Ground truth: ["William Shakespeare", "Roma Gill"]
+Prediction: Roma Gill and W Shakespeare
+Correctness: correct
+Question: Who authored The Taming of the Shrew (published in 2002)?
+Ground truth: ["William Shakespeare", "Roma Gill"]
+Prediction: Roma Shakespeare
+Correctness: incorrect
+Question: What country is Maharashtra Metro Rail Corporation Limited located in?
+Ground truth: ["India"]
+Prediction: Maharashtra
+Correctness: incorrect
+Question: What’s the job of Song Kang-ho in Parasite (2019)?
+Ground truth: ["actor"]
+Prediction: He plays the role of Kim Ki-taek, the patriarch of the Kim
+family.
+Correctness: correct
+Question: 
+"""
+
+
+def model_judge(judge_model, response_model, prompts_judge, access_token):
+  '''
+  Args:
+    judge_model: Hugging Face model name of the model used for judging
+    response_model: Hugging Face model name of the model used for original Q&A responses
+    prompts_judge: dictionary of prompts to be judged by the model
+    access_token: Hugging Face access token
+  Returns:
+    list of the model's judgement replications for each prompt
+  '''
+  tokenizer = AutoTokenizer.from_pretrained(judge_model, token=access_token)
+  model = AutoModelForCausalLM.from_pretrained(judge_model, token=access_token)
+
+  model_judge_dict = {}
+  jud_mod_print_name = judge_model.replace('/', '_')
+  prompt_mod_print_name = response_model.replace('/', '_')
+  for q_dict in prompts_judge['mag']:
+    mod_judge = [generate_judge(model, tokenizer, few_shot_prefix+str(q_dict['question'])+'\n Ground truth: '+str(q_dict['ground_truth'])+'\n Prediction: '+ q_dict['response'][:15]+
+            '\n Correctness: ',
+            0.75, device) for _ in range(100)]
+    model_judge_dict[q_dict['question']] = mod_judge
+  for q_dict in prompts_judge['dblp']:
+    mod_judge = [generate_judge(model, tokenizer, few_shot_prefix+str(q_dict['question'])+'\n Ground truth: '+str(q_dict['ground_truth'])+'\n Prediction: '+ q_dict['response'][:15]+
+            '\n Correctness: ',
+            0.75, device) for _ in range(100)]
+    model_judge_dict[str(q_dict['question'])] = mod_judge
+  with open(f'judge_full_{prompt_mod_print_name}_by_{jud_mod_print_name}.json', 'w') as f:
+    json.dump(model_judge_dict, f)
+  return model_judge_dict
+
+
+judgements_by_mod = {}
+for resp_mod, resp_mod_dict in prompt_resp.items():
+    resp_mod_print_name = resp_mod.replace('/', '_')
+    print(f'Judging responses from {resp_mod_print_name}')
+    judgements_by_mod[resp_mod] = {}
+    for jud_mod in model_types:
+        jud_mod_print_name = jud_mod.replace('/', '_')
+        print(f'Currently judging using {jud_mod_print_name}')
+        judgements_by_mod[resp_mod][jud_mod] = model_judge(resp_mod, jud_mod, resp_mod_dict, access_token)
+
+# Save judgements outputs to json
+with open(f'application_judgements.json', 'w') as f:
+    json.dump(judgements_by_mod, f)
+
+
---
+++ application_data/application_responses.py
@@ -0,0 +1,71 @@
+import json
+import random
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import set_seed
+import torch
+
+from utils import generate_txt
+
+device = "cuda:1"
+
+# Define LLM types
+model_types = ['lmsys/vicuna-7b-v1.5',
+               'tiiuae/falcon-7b']
+               
+# Access token for hugging face
+access_token = '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'
+               
+# Load sampled questions
+with open('head_to_tail_dblp.json', 'r') as dblp:
+    dblp_qs = json.load(dblp)
+with open('head_to_tail_mag.json', 'r') as mag:
+    mag_qs = json.load(mag)
+
+few_shot_prefix = "Answer the following questions in as few words as possible. Say 'unsure' if you don’t know. \n Question: What is the capital of China? \n  Answer: Beijing \n Question: What is the captical of Wernythedia? \n Answer: unsure \n Question: "
+few_shot_suffix = " \n Answer:"
+
+def model_answers(model_name, dblp, mag, access_token):
+  ''' 
+  Args:
+    model_name: Hugging Face model name
+    dblp: list of head to tail questions from DBLP
+    mag: list of head to tail questions from MAG
+    access_token: Hugging Face access token
+  Returns:
+    list of model responses for each prompt
+  '''
+  print(f'Loading model tokenizer for {model_name}.')
+  tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token)
+  print(f'Tokenizer loaded. Loading model {model_name}.')
+  model = AutoModelForCausalLM.from_pretrained(model_name, token=access_token)
+  print(f'Model {model_name} loaded.')
+  mod_print_name = model_name.replace('/', '_')
+  resp_dict = {'mag': [], 'dblp': []}
+  for q in mag['head']:
+    q_resp = generate_txt(model, tokenizer,
+                          few_shot_prefix+q[2]+few_shot_suffix,
+                          seed_val=1234,
+                          device = device)
+    curr_dict = {'question': q[2], 'response': q_resp, 'ground_truth': q[3]}
+    resp_dict['mag'].append(curr_dict)
+  for q in dblp['head']:
+    q_resp = generate_txt(model, tokenizer,
+                          few_shot_prefix+q[2]+few_shot_suffix,
+                          seed_val=1234,
+                          device = device)
+    curr_dict = {'question': q[2], 'response': q_resp, 'ground_truth': q[3]}
+    resp_dict['dblp'].append(curr_dict)
+  with open(f'{mod_print_name}_application_responses.json', 'w') as f:
+    json.dump(resp_dict, f)
+  return resp_dict
+
+
+# Obtain prompts for each model
+model_prompts = {}
+for model in model_types:
+  model_prompts[model] = model_answers(model, dblp_qs, mag_qs, access_token)
+
+# Save outputs to json
+with open('application_responses.json', 'w') as f:
+    json.dump(model_prompts, f)
+
